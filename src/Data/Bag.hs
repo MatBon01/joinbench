730354d9104672b99704430a9781b0640594d949
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-03-14 16:04:55 +0000

    Make Bag a functor

--- src/Data/Bag.hs
+++ src/Data/Bag.hs
@@ -2,3 +2,6 @@ module Data.Bag where
 
 newtype Bag a = Bag { elements :: [a]}
   deriving (Eq, Show)
+
+instance Functor Bag where
+  fmap f (Bag xs) = Bag (fmap f xs)
--- test/Data/BagSpec.hs
+++ test/Data/BagSpec.hs
@@ -5,6 +5,6 @@ import qualified Data.Bag as Bag
 
 spec :: Spec
 spec = do
-  describe "test" $ do
-    it "acts as a placeholder" $ do
-      5 `shouldBe` 5
+  describe "Data.Bag fmap" $ do
+    it "maps over the underlying list" $ do
+      fmap (5 +) (Bag.Bag [0, 10, 20, 30]) `shouldBe` Bag.Bag [5, 15, 25, 35]
