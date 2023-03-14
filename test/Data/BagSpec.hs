6c468e0066c0d7728391685e8d99bbae946bcf48
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-03-14 14:47:18 +0000

    Clean singleton test

--- test/Data/BagSpec.hs
+++ test/Data/BagSpec.hs
@@ -7,4 +7,4 @@ spec :: Spec
 spec = do
   describe "Data.Bags.singleton" $ do
     it "creates a bag with only the element given" $ do
-      (Bag.singleton 1) `shouldBe` (Bag.Bag [1])
+      Bag.singleton 1 `shouldBe` Bag.Bag [1]
