30560fbfba73f99bc2c09c0014c72e39f40f4d8c
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-04-05 14:27:26 +0100

    Implement a Bag data structure (#28)
    * Add record type for bags implementation
    
    * Create a cabal project
    
    * Add DatabaseBagsTest to cabal project
    
    * Add MultiSet to the test database
    
    * Define a table as a MultiSet
    
    * Add newTable function
    
    * Add singletonTable function
    
    * Add union function
    
    * Add test suite for bag package
    
    * Create Bag signature
    
    * Create function for empty
    
    * Clean package structure
    
    * Set up test suite
    
    * Implement and test singleton
    
    * Clean singleton test
    
    * Change bag definiton to match paper supplement
    
    * Make Bag a functor
    
    * Define applicative for bag
    
    * Define Monad for Bag
    
    * Test Applicative for Bags
    
    * Test Monad instance of Bag
    
    * Remove unused ghc pragma
    
    * Add empty function to Bags
    
    * Fix bag equality
    
    * Improve efficiency of eq
    
    * Implement Semigroup for Bag
    
    * Define a commutative monoid
    
    * Define Bag as a monoid
    
    * Define a Bag as a commutative monoid

---
+++ app/DatabaseBagsTest.hs
@@ -0,0 +1,28 @@
+module Main where
+
+import qualified Data.MultiSet as MultiSet
+
+type Name = String
+
+type Age = Int
+
+type Table = MultiSet.MultiSet
+
+type BirthCountry = String
+
+data Record = Record {name :: Name, age :: Age, birthCountry :: BirthCountry}
+  deriving (Show, Eq, Ord)
+
+main :: IO ()
+main = do
+  putStrLn "This is a database bags test"
+  putStrLn (show (MultiSet.insertMany 12 3 MultiSet.empty))
+
+newTable :: Table Record
+newTable = MultiSet.empty
+
+singletonTable :: Record -> Table Record
+singletonTable = MultiSet.singleton
+
+union :: Table Record -> Table Record -> Table Record
+union = MultiSet.union
---
+++ src/Data/Bag.hs
@@ -0,0 +1,41 @@
+module Data.Bag where
+
+import Data.List
+import Data.CMonoid
+
+newtype Bag a = Bag {elements :: [a]}
+  deriving (Show)
+
+instance (Eq a) => Eq (Bag a) where
+  b1 == b2 = eq' (elements b1) (elements b2)
+    where
+      eq' :: (Eq a) => [a] -> [a] -> Bool
+      eq' (x : xs) ys = not (null ys2) && eq' xs (ys1 ++ tail ys2)
+        where
+          (ys1, ys2) = break (== x) ys
+      eq' [] [] = True
+      eq' _ _ = False
+
+instance Functor Bag where
+  fmap f (Bag xs) = Bag (fmap f xs)
+
+instance Applicative Bag where
+  pure x = Bag [x]
+  (<*>) b1 b2 = Bag $ (<*>) (elements b1) (elements b2)
+
+instance Monad Bag where
+  Bag xs >>= k = Bag (xs >>= (elements . k))
+
+instance Semigroup (Bag a) where
+  (<>) = Data.Bag.union
+
+instance Monoid (Bag a) where
+  mempty = Data.Bag.empty
+
+instance CMonoid (Bag a)
+
+empty :: Bag a
+empty = Bag []
+
+union :: Bag a -> Bag a -> Bag a
+b1 `union` b2 = Bag (elements b1 ++ elements b2)
