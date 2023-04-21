57bfa9720eeea0887166dc3301898f5b8fe2471c
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-04-21 18:47:02 +0100

    Define Maybe as a PointedSet

--- src/Data/PointedSet.hs
+++ src/Data/PointedSet.hs
@@ -1,5 +1,11 @@
 module Data.PointedSet where
 
+import Data.Maybe
+
 class PointedSet a where
   null :: a
   isNull :: a -> Bool
+
+instance PointedSet (Maybe a) where
+  null = Nothing
+  isNull = isNothing
