e4f265eb0de5cda0b8e58838e65e81a52fc500b5
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-04-26 17:50:48 +0100

    Start example with cartesian product

--- app/AppendixExample.hs
+++ app/AppendixExample.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Data.Bag
+
 -- Define types used in example
 type Identifier = String
 type Name = String
@@ -8,6 +10,10 @@ type Amount = Float
 
 data Customer = C { cid :: Identifier, name :: Name}
 data Invoice = I { iid :: Identifier, cust :: Identifier, due :: Date, amount :: Amount}
+
+-- Cartesian product of both databases
+exampleWithCP :: Bag Customer -> Bag Invoice -> Bag (Customer, Invoice)
+exampleWithCP cs is = cp cs is
 
 main :: IO ()
 main = do
