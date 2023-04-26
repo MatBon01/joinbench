44aaffd3104a64685b7a8e3ab5111a3c1886fa70
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-04-26 17:25:17 +0100

    Add types for the example

--- app/AppendixExample.hs
+++ app/AppendixExample.hs
@@ -1,5 +1,14 @@
 module Main where
 
+-- Define types used in example
+type Identifier = String
+type Name = String
+type Date = String
+type Amount = Float
+
+data Customer = C { cid :: Identifier, name :: Name}
+data Invoice = I { iid :: Identifier, cust :: Identifier, due :: Date, amount :: Amount}
+
 main :: IO ()
 main = do
   putStrLn "Example from appendix"
