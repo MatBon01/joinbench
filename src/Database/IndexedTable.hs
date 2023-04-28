c58004cbd22354e4548d760ad2b4b66d3e06bbb2
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-04-28 16:33:02 +0100

    Implement projection

--- src/Database/IndexedTable.hs
+++ src/Database/IndexedTable.hs
@@ -11,3 +11,6 @@ singleton (k, v) = Map.single (k, Bag.single v)
 
 union :: (Map.Key k) => Map.Map k (Bag.Bag v) -> Map.Map k (Bag.Bag v) -> Map.Map k (Bag.Bag v)
 union t1 t2 = (fmap (uncurry Bag.union) . Map.merge) (t1, t2)
+
+projection :: (Map.Key k) => (v -> w) -> Map.Map k (Bag.Bag v) -> Map.Map k (Bag.Bag w)
+projection = fmap . fmap
--- test/Database/IndexedTableSpec.hs
+++ test/Database/IndexedTableSpec.hs
@@ -5,6 +5,12 @@ import qualified Database.IndexedTable as Table
 import qualified Data.Key as Map
 import qualified Data.Bag as Bag
 
+type Name = String
+data Person = Person { firstName :: Name, lastName :: Name} deriving (Show, Eq)
+
+people :: Map.Map () (Bag.Bag Person)
+people = Map.Lone (Bag.Bag [Person "John" "Smith", Person "Jane" "Doe", Person "John" "Doe"])
+
 spec :: Spec
 spec = do
   describe "empty" $ do
@@ -20,3 +26,10 @@ spec = do
       Table.union (Table.empty :: Map.Map () (Bag.Bag Char)) (Table.singleton ((), 'a')) `shouldBe` Map.Lone (Bag.Bag ['a'])
     it "can correctly deal with second element empty" $ do
       Table.union (Table.singleton ((), 'a')) (Table.empty :: Map.Map () (Bag.Bag Char)) `shouldBe` Map.Lone (Bag.Bag ['a'])
+  describe "projection" $ do
+    it "can correctly do a general projection" $ do
+      Table.projection  firstName people `shouldBe` Map.Lone (Bag.Bag ["John", "Jane", "John"])
+    it "can correctly project on an empty map" $ do
+      Table.projection lastName (Table.empty :: Map.Map () (Bag.Bag Person)) `shouldBe` Map.empty
+    it "can correctly use the identity projection" $ do
+      Table.projection id people `shouldBe` people
