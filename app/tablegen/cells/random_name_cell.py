dc1136838a94b325499aee641dd7a6ee233f71ff
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-05-08 14:21:50 +0100

    Add name list to random name generator constructor

--- app/tablegen/cells/random_name_cell.py
+++ app/tablegen/cells/random_name_cell.py
@@ -1,6 +1,10 @@
 from cells.cell import Cell
 import random
+from typing import List
 
 class RandomNameCell(Cell):
+    def __init__(self, names: List[str]):
+        self.names: List[str] = names
+
     def generate(self) -> str:
-        return random.choice(["Alice", "Bob", "Charlie"])
+        return random.choice(self.names)
