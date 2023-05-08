9c81d06572fdb7c3985abd7d36f36d10c09fc4e1
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-05-08 14:30:38 +0100

    Add optional bounds on an id

--- app/tablegen/cells/id_cell.py
+++ app/tablegen/cells/id_cell.py
@@ -2,5 +2,12 @@ from cell import Cell
 import random
 
 class IdCell(Cell):
+    def __init__(self, lower_bound: int = 10000000, upper_bound: int = 99999999):
+        self.lower_bound: int = lower_bound
+        self.upper_bound: int = upper_bound
+
+    def generate_id(self) -> int:
+        return random.randint(self.lower_bound, self.upper_bound)
+
     def generate(self) -> str:
-        return str(random.randint(1, 1000))
+        return str(self.generate_id())
