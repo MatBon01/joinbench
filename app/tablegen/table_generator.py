d5d2bb595ba7c9760c8291ec02479e17fe0c3804
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-05-09 15:17:11 +0100

    Remove num of records from table gen init

--- app/tablegen/table_generator.py
+++ app/tablegen/table_generator.py
@@ -1,10 +1,9 @@
 from .record_generator import RecordGenerator
 from typing import List, Iterable
 
-class table_generator:
-    def __init__(self, record_generator: RecordGenerator, num_of_records: int):
+class TableGenerator:
+    def __init__(self, record_generator: RecordGenerator):
         self.record_generator: RecordGenerator = record_generator
-        self.num_of_records: int = num_of_records
 
     def generate(self, num_records: int = 1) -> Iterable[List[str]]:
         for _ in range(num_records):
