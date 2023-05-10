41143751508adaa96c3d243e93ec885bd2335bc5
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-05-10 15:38:12 +0100

    Make attempt checking more readable

--- app/tablegen/cells/unique_cell.py
+++ app/tablegen/cells/unique_cell.py
@@ -8,11 +8,13 @@ class UniqueCell(Cell):
         self.seen_values: Set[str] = set()
         self.MAX_ATTEMPTS: Final[int] = max_attempts
 
+    def has_remaining_attempts(self, curr_attempt: int) -> bool:
+        return self.MAX_ATTEMPTS == -1 or curr_attempt < self.MAX_ATTEMPTS
+
     def generate(self) -> str:
         curr_attempt: int = 0
         value: str = self.cell.generate()
-        while value in self.seen_values and (curr_attempt < self.MAX_ATTEMPTS
-                                             or self.MAX_ATTEMPTS == -1):
+        while value in self.seen_values and self.has_remaining_attempts(curr_attempt):
             value = self.cell.generate()
         self.seen_values.add(value)
         return value
