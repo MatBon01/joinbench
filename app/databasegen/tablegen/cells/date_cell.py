47a6e4119c52267617450d8b8f090ac7debb8e6c
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-05-27 13:40:22 +0100

    Implement month generation

--- app/databasegen/tablegen/cells/date_cell.py
+++ app/databasegen/tablegen/cells/date_cell.py
@@ -12,5 +12,11 @@ class DateCell(Cell):
     def generate_year(self) -> str:
         return str(self.random.randint(self.year_lower, self.year_upper))
 
+    def generate_month(self) -> str:
+        # Assume each month is an integer from 1 to 12
+        FIRST_MONTH: int = 1
+        LAST_MONTH: int = 12
+        return str(self.random.randint(FIRST_MONTH, LAST_MONTH))
+
     def generate(self) -> str:
         raise NotImplementedError("Not all components of date generated")
