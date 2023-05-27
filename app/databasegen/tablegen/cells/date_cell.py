b076df371544def83d2ad075c9c5a75719514870
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-05-27 14:05:31 +0100

    Define month enum

--- app/databasegen/tablegen/cells/date_cell.py
+++ app/databasegen/tablegen/cells/date_cell.py
@@ -1,6 +1,22 @@
+from enum import IntEnum
 from random import Random
 
 from tablegen.cells.cell import Cell
+
+
+class Month(IntEnum):
+    JANUARY = 1
+    FEBRUARY = 2
+    MARCH = 3
+    APRIL = 4
+    MAY = 5
+    JUNE = 6
+    JULY = 7
+    AUGUST = 8
+    SEPTEMBER = 9
+    OCTOBER = 10
+    NOVEMBER = 11
+    DECEMBER = 12
 
 
 class DateCell(Cell):
@@ -13,10 +29,10 @@ class DateCell(Cell):
         return str(self.random.randint(self.year_lower, self.year_upper))
 
     def generate_month(self) -> str:
-        # Assume each month is an integer from 1 to 12
-        FIRST_MONTH: int = 1
-        LAST_MONTH: int = 12
-        return str(self.random.randint(FIRST_MONTH, LAST_MONTH))
+        return str(self.random.randint(Month.JANUARY, Month.DECEMBER))
+
+    def generate_day(self, month: int, year: int) -> str:
+        raise NotImplementedError()
 
     def generate(self) -> str:
         raise NotImplementedError("Not all components of date generated")
