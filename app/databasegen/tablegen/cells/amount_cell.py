b30264eaf606f5078ee455748f311e698b23b223
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-05-29 15:35:40 +0100

    Fix incorrect type for amount generation

--- app/databasegen/tablegen/cells/amount_cell.py
+++ app/databasegen/tablegen/cells/amount_cell.py
@@ -3,11 +3,13 @@ from random import Random
 from tablegen.cells.cell import Cell
 
 
+Amount = int
+
 class AmountCell(Cell):
-    def __init__(self, random: Random, lower: float = 0.01, upper: float = 9999.99):
+    def __init__(self, random: Random, lower: Amount = 1, upper: Amount = 999999):
         self.random: Random = random
-        self.lower: float = lower
-        self.upper: float = upper
+        self.lower: Amount = lower
+        self.upper: Amount = upper
 
     def generate(self) -> str:
-        return str(self.random.uniform(self.lower, self.upper))
+        return str(self.random.randint(self.lower, self.upper))
