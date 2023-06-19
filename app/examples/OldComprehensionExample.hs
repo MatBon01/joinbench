d3532feaa154a4e1fed2aefc3b3be606f81c47f5
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-06-19 16:45:03 +0100

    Read in both tables correctly for old comprehension example

--- app/examples/OldComprehensionExample.hs
+++ app/examples/OldComprehensionExample.hs
@@ -9,4 +9,8 @@ main :: IO ()
 main = do
   customersCSV <- readFile "app/examples/tables/c100.csv"
   let customers = fromRight empty $ CP.parseCSV customersCSV
+  invoicesCSV <- readFile "app/examples/tables/i100.csv"
+  let invoices = fromRight empty $ IP.parseCSV invoicesCSV
   print $ head $ elements customers
+  print $ head $ elements invoices
+
