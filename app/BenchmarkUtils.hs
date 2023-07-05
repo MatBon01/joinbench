98f901f382e60fc9d6f15524afeb3b6a031ca023
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-07-05 19:16:57 +0100

    Refactor out equijoin methods

--- app/BenchmarkUtils.hs
+++ app/BenchmarkUtils.hs
@@ -1,12 +1,21 @@
+{-# LANGUAGE ExplicitForAll #-}
+
 module BenchmarkUtils where
 
 import Criterion.Main
+import Data.Key
 import Database.Bag
 
 equijoinBenchmark name fa fb as bs =
     bgroup
         name
-        [ bench "modular product" $ whnf (productEquijoin fa fb) (as, bs)
-        , bench "old comprehension" $ whnf (comprehensionEquijoin fa fb) (as, bs)
-        , bench "modular indexed" $ whnf (indexedEquijoin fa fb) (as, bs)
-        ]
+        (map equijoinTransform equijoinNameAndMethod)
+  where
+    equijoinTransform (name, func) = bench name $ whnf (func fa fb) (as, bs)
+
+equijoinNameAndMethod :: forall k a b. (Eq k, Key k) => [(String, (a -> k) -> (b -> k) -> (Table a, Table b) -> Table (a, b))]
+equijoinNameAndMethod =
+    [ ("modular product", productEquijoin)
+    , ("old comprehension", comprehensionEquijoin)
+    , ("modular indexed", indexedEquijoin)
+    ]
