80d535b1626649a766ecfef2365b6dc7631d2fe8
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-07-06 12:33:23 +0100

    Add a commandline argument for benchmark name

--- app/JoinBenchBenchmark.hs
+++ app/JoinBenchBenchmark.hs
@@ -6,21 +6,30 @@ import BenchmarkUtils
 import Criterion.Main
 import Data.Either
 import Database.Bag
+import System.Environment
 import Text.Parser.JoinBenchTable
+
+numArgs = 1
+joinBenchFileNameArgIndex = 0
 
 main :: IO ()
 main = do
-    tableName <- getJoinBenchTableName
-    table <- getJoinBenchTable tableName
-    defaultMain
-        [ equijoinBenchmark "join on onePercent" onePercent onePercent table table
-        , equijoinBenchmark "join onePercent and twentyPercent" onePercent twentyPercent table table
-        , equijoinBenchmark "join onePercent and fiftyPercent" onePercent fiftyPercent table table
-        , equijoinBenchmark "join even and odd" evenOnePercent oddOnePercent table table
-        ]
+    joinBenchFileName <- getJoinBenchTableName
+    table <- getJoinBenchTable joinBenchFileName
+
+    args <- getArgs
+    withArgs (drop numArgs args) $
+        defaultMain
+            [ equijoinBenchmark "join on onePercent" onePercent onePercent table table
+            , equijoinBenchmark "join onePercent and twentyPercent" onePercent twentyPercent table table
+            , equijoinBenchmark "join onePercent and fiftyPercent" onePercent fiftyPercent table table
+            , equijoinBenchmark "join even and odd" evenOnePercent oddOnePercent table table
+            ]
 
 getJoinBenchTableName :: IO String
-getJoinBenchTableName = return "tables/join_bench_table.csv"
+getJoinBenchTableName = do
+    args <- getArgs
+    return $ args !! joinBenchFileNameArgIndex
 
 getJoinBenchTable :: String -> IO (Table JoinBenchRecord)
 getJoinBenchTable joinBenchFileName = do
