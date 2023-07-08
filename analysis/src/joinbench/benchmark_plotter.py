b4352da4830e1b5f08d44b58ee0c974256ed5306
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-07-08 12:00:18 +0100

    Add mean plot to plotter

--- analysis/src/joinbench/benchmark_plotter.py
+++ analysis/src/joinbench/benchmark_plotter.py
@@ -1,6 +1,27 @@
-from joinbench.benchmark_data import BenchmarkData
+from typing import Dict, List, Tuple
+
+from .benchmark_data import BenchmarkData
 
 
 class BenchmarkPlotter:
     def __init__(self, data: BenchmarkData):
         self.data = data
+
+    def plot_benchmark_group_means(self, ax, group_name: str):
+        group_map: Dict[
+            str, List[Tuple[int, str]]
+        ] = self.data.map_benchmark_groups_and_benchmark_indices()
+        experiments: List[Tuple[int, str]] = group_map[group_name]
+
+        indices: List[int]
+        names: List[str]
+        indices, names = zip(*experiments)
+        means = self.data.get_means_of_benchmark_list(indices)
+
+        ax.set_title(f"Mean time to complete '{group_name}'")
+        ax.set_ylabel("time (s)")
+        ax.set_xlabel("function")
+
+        ax.bar(names, means)
+
+        return ax
