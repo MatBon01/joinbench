014cb7df593ab9bcf33b9a9893b82a880e01cd41
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-07-09 12:55:40 +0100

    Define plotter for evaluation methods

---
+++ analysis/src/joinbench/evaluation_method_plotter.py
@@ -0,0 +1,44 @@
+from typing import List, Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from .benchmark_data import BenchmarkData
+
+
+class EvaluationMethodPlotter:
+    def __init__(self, nf_data: BenchmarkData, whnf_data: BenchmarkData):
+        self.nf_data = nf_data
+        self.whnf_data = whnf_data
+
+    def plot_benchmark_group_means_comparison(self, ax, group_name: str):
+        whnf_map = self.whnf_data.map_benchmark_groups_and_benchmark_indices()
+
+        indices: List[int]
+        experiment_names: List[str]
+        experiments: List[Tuple[int, str]] = whnf_map[group_name]
+        indices, experiment_names = zip(*experiments)
+
+        evaluation_method_means = {
+            "nf": self.nf_data.get_means_of_benchmark_list(indices),
+            "whnf": self.whnf_data.get_means_of_benchmark_list(indices),
+        }
+
+        bar_width = 0.25
+        method_pos = 0
+
+        experiment_positions = np.arange(len(experiments))
+        for method, mean in evaluation_method_means.items():
+            offset = bar_width * method_pos
+            ax.bar(experiment_positions + offset, mean, bar_width, label=method)
+            method_pos += 1
+
+        ax.set_ylabel("Time (s)")
+        ax.set_xlabel("Function")
+        ax.set_title(
+            f"Mean time to complete '{group_name}' by evaluation method"
+        )
+        ax.set_xticks(experiment_positions + bar_width, experiment_names)
+        ax.legend(title="Evaluation method", loc="upper right", ncols=3)
+
+        return ax
