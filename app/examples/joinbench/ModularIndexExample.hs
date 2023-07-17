a532e93fb8899a36d68bc3876482cce7ee2ca347
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-07-17 16:27:44 +0100

    Add joinbench modular index example profiling

--- app/examples/joinbench/ModularIndexExample.hs
+++ app/examples/joinbench/ModularIndexExample.hs
@@ -3,8 +3,10 @@ module Main where
 import Database.Bag
 import Text.Parser.JoinBenchTable
 
+strictPair (a, b) = a `seq` b `seq` (a, b)
+
 main = do
-    joinbench <- getJoinBenchTable "tables/join_bench_table_100.csv"
+    joinbench <- getJoinBenchTable "tables/join_bench_table_5000.csv"
 
     let joinedOnePercentWithFiftyPercent = indexedEquijoin onePercent fiftyPercent (joinbench, joinbench)
 
