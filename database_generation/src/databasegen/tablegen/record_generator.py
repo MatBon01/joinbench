9d69281251a9bc443ac89912c6bf7db979b35dac
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-07-31 20:42:20 +0100

    Add information on synthetic database creation (#85)
    * Outline sections in benchmarking chapter
    
    * Add some text to benchmark introduction
    
    * Explain the advantages of a synthetic database
    
    * Add reference to benchmarking best practices
    
    * Write introduction to work in synthetic database creation
    
    * Add an overview of the synthetic database code
    
    * Add missing type annotation to record_generator
    
    * Add class diagram to help synthetic database explanation
    
    * Add an introduction to the Cell abstract class
    
    * Add section for RecordGenerator
    
    * Describe the TableGenerator class
    
    * Describe outputting the data
    
    * Add draft uml diagram for databasegen package
    
    * Add classes and types to dictionary

--- database_generation/src/databasegen/tablegen/record_generator.py
+++ database_generation/src/databasegen/tablegen/record_generator.py
@@ -5,7 +5,7 @@ from .cells.cell import Cell
 
 class RecordGenerator:
     def __init__(self, cells: List[Cell]):
-        self.cells = cells
+        self.cells: List[Cell] = cells
 
     def generate(self) -> List[str]:
         return list(map(lambda cell: cell.generate(), self.cells))
